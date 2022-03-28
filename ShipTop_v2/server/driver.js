5a188719ce857b5ce6c90504e0cd3fb1c5e0fea7
Abdulrahman Alomar <101531757+alomar-99@users.noreply.github.com>
2022-03-28 17:56:57 +0300

    update

--- ShipTop_v2/server/driver.js
+++ ShipTop_v2/server/driver.js
@@ -1,3 +1,25 @@
-class Driver {
+const conn = require('./accessibility/connection');
+const utility = require('./accessibility/utility');
+
+const transprotationMember = require('./tranportationMember');
+
+class Driver  extends tranportationMember.tranportationMember{
+    constructor(driverID, firstName, lastName, email, phoneNumber, password){
+        super(firstName, lastName, email, phoneNumber,password);
+        this.driverID = driverID;
+    }
+
+
+
+    viewAssignedVehicle(driver){
+
+    }
+    
+
+
+
+
+
+
     
 }
---
+++ ShipTop_v2/server/transportationMember.js
@@ -0,0 +1,23 @@
+const conn = require('./accessibility/connection');
+const utility = require('./accessibility/utility');
+const employee = require('./employee');
+class transprotationMember extends employee.Employee{
+    constructor( firstName, lastName, email, phoneNumber, password){
+        super(firstName, lastName, email, phoneNumber,password);
+        
+    }
+
+    viewShipmentDetails(shipment){
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
