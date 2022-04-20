a3645baa82c4e8308e600c135a1d8b4d23e1fecd
Abdulrahman Alomar <101531757+alomar-99@users.noreply.github.com>
2022-04-21 02:11:15 +0300

    33
    aa

--- ShipTop_v2/server.js
+++ ShipTop_v2/server.js
@@ -59,7 +59,7 @@ app.post("/api/logisticManager/addWarehouseManager",urlEncodedParser, (req, res)
             });
         else{
             today = time.getDateTime();
-            const sql = "INSERT INTO warehousemanagers(firstName, lastName, email, phoneNumber, password, warehouseID, lastUpdate)"+ "values('"+ req.body.firstName +"', '" + req.body.lastName + "', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password + "', " + req.body.warehouseID +", '"+ today+"')"
+            const sql = "INSERT INTO warehousemanagers(firstName, lastName, email, phoneNumber, password, warehouseID, updatedBy, lastUpdate)"+ "values('"+ req.body.firstName +"', '" + req.body.lastName + "', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password + "', " + req.body.warehouseID +", 'LM" + req.body.employeeID +"', '"+ today+"')";
             connection.query(sql, (err)=>{
             if (err) {
                 console.log(err);
@@ -70,10 +70,9 @@ app.post("/api/logisticManager/addWarehouseManager",urlEncodedParser, (req, res)
                 "err": false
             });
             });
-            const employeeSQL = "INSERT INTO employees(firstName, lastName, email, phoneNumber, password, updatedBy, lastUpdate)"+ "values('"+ req.body.firstName +"', '" + req.body.lastName + "', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password + "', 'LM" + req.body.employeeID +"', '"+ today+"')"
+            const employeeSQL = "INSERT INTO employees(firstName, lastName, email, phoneNumber, password, updatedBy, lastUpdate)"+ "values('"+ req.body.firstName +"', '" + req.body.lastName + "', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password + "', 'LM" + req.body.employeeID +"', '"+ today+"')";
             connection.query(employeeSQL, (err)=>{
                 if (err) {
-                    console.log(employeeSQL);
                     console.log(err);
                     throw err;
                 }
@@ -98,7 +97,7 @@ app.post("/api/logisticManager/modifyWarehouseManager",urlEncodedParser, (req, r
             });
         else{
             today = time.getDateTime();
-            const sql = "UPDATE warehousemanagers SET (firstName, lastName, email, phoneNumber, password, warehouseID, lastUpdate)"+ "values('"+ req.body.firstName +"', '" + req.body.lastName + "', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password + "', " + req.body.warehouseID +", '"+ today+"')"
+            const sql = "UPDATE warehousemanagers SET email = '"+req.body.email+"', phoneNumber = '" + req.body.phoneNumber + "', password = '"+req.body.password + "', updatedBy = 'LM"+req.body.employeeID+"', lastUpdate = '"+today+"' WHERE warehouseManagerID = "+req.body.warehouseManagerID+"'";
             connection.query(sql, (err)=>{
             if (err) {
                 console.log(err);
@@ -109,7 +108,7 @@ app.post("/api/logisticManager/modifyWarehouseManager",urlEncodedParser, (req, r
                 "err": false
             });
             });
-            const employeeSQL = " UPDATE employees SET(firstName, lastName, email, phoneNumber, password, updatedBy, lastUpdate)"+ "values('"+ req.body.firstName +"', '" + req.body.lastName + "', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password + "', 'LM" + req.body.employeeID +"', '"+ today+"')"
+            const employeeSQL = "UPDATE employees SET email = '"+req.body.email+"', phoneNumber = '" + req.body.phoneNumber + "', password = '"+req.body.password + "', updatedBy = '"+req.body.employeeID+"', lastUpdate = '"+today+"' WHERE warehouseManagerID = WM"+req.body.employeeID+"'";
             connection.query(employeeSQL, (err)=>{
                 if (err) {
                     console.log(employeeSQL);
@@ -131,10 +130,227 @@ app.post("/api/logisticManager/modifyWarehouseManager",urlEncodedParser, (req, r
 
 
 
+let example = {
+    "shipmentID": "",
+
+}
+
 //DRIVER //////////////////////////////////////////////
+app.post("/api/driver/viewassinedShipment",urlEncodedParser, (req, res) => {
+    const errSQL = "SELECT * FROM shipments WHERE currentPlace ='" +req.body.currentPlace +"'";
+    connection.query(errSQL, (err, result)=>{
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        if (result=="")    
+            res.send({
+                "status": "no shipment", 
+                "err": true
+            });
+        else{
+
+    
+            connection.query(errSQL, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send(JSON.stringify(result));
+
+            //{
+
+            //   "status": "SUCCESS", 
+            //    "err": false
+            //}
+            });
+        }
+    });
+});
+
+app.post("/api/driver/viewListShipments",urlEncodedParser, (req, res) => {
+    const errSQL = "SELECT * FROM shipments ";
+    connection.query(errSQL, (err, result)=>{
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+       
+        if (result=="")    
+            res.send({
+                "status": "no EXISTING shipments", 
+                "err": true
+            });
+        else{
+
+    
+            connection.query(errSQL, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send(JSON.stringify(result));
+
+            //{
+
+            //   "status": "SUCCESS", 
+            //    "err": false
+            //}
+            });
+        }
+    });
+});
 
 
 
+
+
+let example4 = {
+    "shipmentID": "1",
+    "status": "deleverd",
+}
+app.post("/api/driver/updateShipmentStatus",urlEncodedParser, (req, res) => {
+    let sql = "UPDATE shipments SET 'status' = "+req.body.status+" where'shipmentID' = "+req.body.shipmentId;
+            connection.query(sql, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send(
+            {
+              "status": "SUCCESS", 
+               "err": false
+            }
+            );
+            });    
+});
+
+
+
+////////////////dispatcher////////////
+
+app.post("/api/dispatcher/addShipment",urlEncodedParser, (req, res) => {
+    today = time.getDateTime();
+    const sql = "INSERT INTO `shipments`( `destinationNumber`, `weight`, `description`, `length`, `width`, `height`, `isBreakable`, `status`, `telephone`, `department`, `address`, `sender`, `departureCity`, `arrivalCity`, `arrivalDate`, `deliveryDate`, `warehouseID`, `updatedBy`, `lastUpdate`)"+
+     "VALUES ("+req.body.destinationNumber +"," +req.body.weight +",'" +req.body.description +"'," +req.body.length +"," +req.body.width +"," +req.body.height +"," +req.body.isBreakable +"," +req.body.status +",'" +req.body.telephone +"','" +req.body.department +"','" +req.body.address +"','" +req.body.sender +"','" +req.body.departureCity +"','"+req.body.arrivalCity+"','"+req.body.arrivalDate +"','"+req.body.deliveryDate+"','"+ req.body.warehouseID+"','"+req.body.updatedBy+"','"+today+"')";
+    
+ 
+         
+            
+             connection.query(sql, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send({
+                "status": "SUCCESS", 
+                "err": false
+            });
+            });
+            
+        
+    
+});
+app.post("/api/dispatcher/viewShipment",urlEncodedParser, (req, res) => {
+    const errSQL = "SELECT * FROM shipments WHERE currentPlace ='" +req.body.currentPlace +"'";
+    connection.query(errSQL, (err, result)=>{
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        if (result=="")    
+            res.send({
+                "status": "no shipment", 
+                "err": true
+            });
+        else{
+
+    
+            connection.query(errSQL, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send(JSON.stringify(result));
+
+            //{
+
+            //   "status": "SUCCESS", 
+            //    "err": false
+            //}
+            });
+        }
+    });
+});
+app.post("/api/dispatcher/addVehicle",urlEncodedParser, (req, res) => {
+    today = time.getDateTime();
+    const sql = "INSERT INTO `vehicles`( `vehicleName`, `manufacturerCompany`, `status`, `address`, `assignedDriver`, `dispatcherID`, `lastUpdate`) ";
+    +"VALUES ('" +req.body.vehicleName +"','" +req.body.manufacturerCompany +"','" +req.body.status +"',\'" +req.body.address +"',\'" +req.body.assignedDriver +"',\'" +req.body.dispatcherID +"',\'"+today+"')";
+   
+             connection.query(sql, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send({
+                "status": "SUCCESS", 
+                "err": false
+            });
+            });
+        
+});
+
+app.post("/api/driver/updateVehicleStatus",urlEncodedParser, (req, res) => {
+    today = time.getDateTime();
+    let sql = "UPDATE vehicles SET 'status' = '"+req.body.status+"','lastUpdate' = "+today+" where'vehicleID' = "+req.body.vehicleId;
+            connection.query(sql, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send(
+            {
+              "status": "SUCCESS", 
+               "err": false
+            }
+            );
+            });    
+});
+app.post("/api/driver/editVehicle",urlEncodedParser, (req, res) => {
+    today = time.getDateTime();
+    let sql = "UPDATE vehicles SET 'address' = '"+req.body.address+"','lastUpdate' = "+today+",'vehicleName' = '"+req.body.vehicleName+"','assignedDriver' = "+req.body.status+", where'vehicleID' = "+req.body.vehicleId;
+            connection.query(sql, (err)=>{
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+            res.send(
+            {
+              "status": "SUCCESS", 
+               "err": false
+            }
+            );
+            });    
+});
+
+
+
+
+
+
+
+
+
+
+let example2 = {
+    "employeeID": "75",
+    "firstName": "Ahmed",
+    "lastName": "Khalid",
+    "email": "k84734@gmail.com",
+    "phoneNumber": "021846544",
+    "password": "hud72947",
+    "warehouseID": "99851"
+}
 
 
 //listening
