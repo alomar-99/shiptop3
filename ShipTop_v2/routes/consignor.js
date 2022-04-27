d2e32a4d108fcfbd6b6c209a43d17dc9a9dcc56c
KhalidAlmubarak <67612309+KhalidAlmubarak@users.noreply.github.com>
2022-04-28 01:59:02 +0300

    update consignor

--- ShipTop_v2/routes/consignor.js
+++ ShipTop_v2/routes/consignor.js
@@ -28,5 +28,40 @@ router.post("/addOrder",urlEncodedParser,(req,res)=>{
 });
 
 
+//add Rating
+router.post("/addconsignorrate", urlEncodedParser, (req, res) => {
+    const checkIDSQL = "SELECT * FROM employee WHERE employeeID ='" + req.body.consignorID + "'";
+    DB.query(checkIDSQL, (err, result) => {
+        if (err) throw err;
+        if (result != "") {
 
+            let rateSQL = "START TRANSACTION; \n";
+            rateSQL += "update  consignorrate set rate='" + req.body.rate + "', comment = '" + req.body.comment + "' WHERE employeeID = " + req.body.employeeID;
+            DB.query(rateSQL, (err) => {
+                if (err) throw err;
+                res.send({
+                    "status": "SUCCESS",
+                    "err": false
+                });
+            });
+        }
+
+        else {
+
+            //INSERT INTO  consignorrate (consignorID, rate, comment) values(req.body.consignorID,req.body.rate,req.body.comment);
+
+            let rateSQL = "START TRANSACTION; \n";
+            rateSQL += "INSERT INTO consignorrate (consignorID, rate, comment) VALUES('" + req.body.consignorID + "'," + req.body.rate, req.body.comment + "'); \n";
+            DB.query(rateSQL, (err) => {
+                if (err) throw err;
+                res.send({
+                    "status": "SUCCESS",
+                    "err": false
+                });
+            });
+
+
+        }
+    });
+});
 module.exports = router;
