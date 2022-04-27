8ca5f0e439bc67cad8901ab5f8357c971e8cda21
Abdulrahman Alomar <101531757+alomar-99@users.noreply.github.com>
2022-04-27 04:04:37 +0300

    .
    .

--- ShipTop_v2/routes/administrator.js
+++ ShipTop_v2/routes/administrator.js
@@ -4,7 +4,7 @@ const DB = require('./tools/config').connection;
 const time = require('./tools/utility');
 const urlEncodedParser = require('./tools/config').middleware;
 
-//add warehouse manager
+//add logistic manager
 router.post("/addlogisticManager",urlEncodedParser, (req, res) => {
     const checkEmailSQL = "SELECT * FROM employee WHERE email ='" +req.body.email +"'";
     DB.query(checkEmailSQL, (err, result)=>{
@@ -41,6 +41,45 @@ router.post("/addlogisticManager",urlEncodedParser, (req, res) => {
         }
     });
 });
+///////////////
+//add Freight Broker
+router.post("/addfreightBroker",urlEncodedParser, (req, res) => {
+    const checkEmailSQL = "SELECT * FROM employee WHERE email ='" +req.body.email +"'";
+    DB.query(checkEmailSQL, (err, result)=>{
+        if (err) throw err;
+        if (result!="")     
+            res.send({
+                "status": "EXISTING ACC", 
+                "err": true 
+            }); 
+        else{
+            const checkLocationSQL = "SELECT employeeID FROM office WHERE roomNumber = " +req.body.office.roomNumber+" AND location = '" + req.body.office.location +"'";
+            DB.query(checkLocationSQL, (err,result)=>{
+                if (err) throw err;
+                if (result!="")
+                    res.send({
+                        "status": "DUPLICATE LOC", 
+                        "err": true 
+                    });
+                else{
+                    let employeeSQL = "START TRANSACTION; \n";
+                    employeeSQL += "INSERT INTO employee\n (firstName, lastName, role, email, phoneNumber, password)\n VALUES('"+ req.body.firstName +"', '" + req.body.lastName + "', 'FB', '" + req.body.email + "', '" + req.body.phoneNumber + "', '" + req.body.password +"'); \n";
+                    employeeSQL += "INSERT INTO office\n (employeeID, location, telephone, roomNumber)\n VALUES((SELECT employeeID FROM employee WHERE email = '"+req.body.email+"') ,'" + req.body.office.location + "', '" + req.body.office.telephone + "', "+req.body.office.roomNumber+"); \n";
+                    employeeSQL += "INSERT INTO employeeupdate\n (employeeID, updatedBy, lastUpdate)\n VALUES((SELECT employeeID FROM employee WHERE email = '"+req.body.email+"') ," + req.body.employeeID + ", '" + time.getDateTime() + "'); \n";
+                    employeeSQL += "COMMIT; "
+                    DB.query(employeeSQL, (err)=>{
+                        if (err) throw err;
+                        res.send({
+                            "status": "SUCCESS",
+                            "err": false
+                        });
+                    });
+                }
+            });
+        }
+    });
+});
+
 
 
 module.exports = router;
