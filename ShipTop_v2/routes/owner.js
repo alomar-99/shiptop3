4fc3fd7de88bb330e03cad25bbda2530ff7ab109
Abdulrahman Alomar <101531757+alomar-99@users.noreply.github.com>
2022-05-09 11:45:59 +0300

    owner fixes
    .

--- ShipTop_v2/routes/owner.js
+++ ShipTop_v2/routes/owner.js
@@ -6,10 +6,11 @@ const urlEncodedParser = require('./tools/config').middleware;
 const ownerCheck = require('./tools/utility').isOwner;
 
 //add Administrator
-router.post("/addadministrator",urlEncodedParser, (req, res) => {
+router.post("/addAdministrator",urlEncodedParser, (req, res) => {\
+    //check if this admin is an owner
     DB.query(ownerCheck(req.body.employeeID), (err, result)=>{
         if (err) throw err;
-        if (!result[0].isOwner)
+        if (!result[0].IsOwner)
         res.send({
             "status": "NOT OWNER", 
             "err": true 
@@ -54,74 +55,96 @@ router.post("/addadministrator",urlEncodedParser, (req, res) => {
 });
 
 //delete Administrator
-router.post("/deleteadministrator",urlEncodedParser, (req, res) => {
-    const errSQL = "SELECT * FROM employee WHERE employeeID = " +req.body.adminID;
-    DB.query(errSQL, (err, result)=>{
+router.post("/deleteAdministrator",urlEncodedParser, (req, res) => {
+    //check if this admin is an owner
+    DB.query(ownerCheck(req.body.employeeID), (err, result)=>{
         if (err) throw err;
-        if (result=="")    
-            res.send({
-                "status": "ACC DOESN't EXIST", 
-                "err": true
-            });
+        if (!result[0].IsOwner)
+        res.send({
+            "status": "NOT OWNER", 
+            "err": true 
+        });
         else{
-            const employeeSQL = "DELETE FROM employee WHERE employeeID = "+req.body.adminID ;
-            DB.query(employeeSQL, (err)=>{
+            const errSQL = "SELECT * FROM employee WHERE employeeID = " +req.body.adminID;
+            DB.query(errSQL, (err, result)=>{
                 if (err) throw err;
-                res.send({
-                    "status": "SUCCESS", 
-                    "err": false
-                }); 
+                if (result=="")    
+                    res.send({
+                        "status": "ACC DOESN't EXIST", 
+                        "err": true
+                    });
+                else{
+                    const employeeSQL = "DELETE FROM employee WHERE employeeID = "+req.body.adminID ;
+                    DB.query(employeeSQL, (err)=>{
+                        if (err) throw err;
+                        res.send({
+                            "status": "SUCCESS", 
+                            "err": false
+                        }); 
+                    });
+                }         
             });
         }
     });
+    
 });
 
 //modify Administrator
 router.post("/modifyAdministrator",urlEncodedParser, (req, res) => {
-    const checkIDSQL = "SELECT * FROM employee WHERE employeeID = " +req.body.adminID;
-    DB.query(checkIDSQL, (err, result)=>{
+    //check if this admin is an owner
+    DB.query(ownerCheck(req.body.employeeID), (err, result)=>{
         if (err) throw err;
-        let employeeSQL="";
-        if (result=="")    
-            res.send({ 
-                "status": "ACC DOESN't EXIST", 
-                "err": true
-            }); 
+        if (!result[0].IsOwner)
+        res.send({
+            "status": "NOT OWNER", 
+            "err": true 
+        });
         else{
-            const checkLocationSQL = "SELECT * FROM office WHERE roomNumber = " +req.body.office.roomNumber+" AND location = '" + req.body.office.location +"' AND employeeID !=" +req.body.adminID;
-            DB.query(checkLocationSQL, (err,result)=>{
+            const checkIDSQL = "SELECT * FROM employee WHERE employeeID = " +req.body.adminID;
+            DB.query(checkIDSQL, (err, result)=>{
                 if (err) throw err;
-                console.log(result);
-                if (result!="")
-                    res.send({
-                        "status": "DUPLICATE LOC", 
-                        "err": true 
-                    });
-                else{ 
-                    const checkEmailSQL = "SELECT employeeID FROM employee WHERE email ='" +req.body.email +"' AND employeeID !="+req.body.adminID;
-                    DB.query(checkEmailSQL, (err, result)=>{ 
-                    if (err) throw err;  
-                    if(result[0]!=undefined)
-                        res.send({
-                            "status": "DUPLICATE EMAIL", 
-                            "err": true
-                        });
-                    else{
-                        employeeSQL+= "START TRANSACTION; \n" 
-                        employeeSQL+= "UPDATE employee \n SET phoneNumber = '" + req.body.phoneNumber + "', password = '"+ req.body.password +"'\n WHERE employeeID = "+req.body.adminID + ";\n";
-                        employeeSQL+= "UPDATE employeeupdate \n SET updatedBy = " + req.body.employeeID + ", lastUpdate = '"+ time.getDateTime() +"'\n WHERE employeeID = "+req.body.adminID + ";\n";
-                        employeeSQL+= "UPDATE office \n SET location = '" + req.body.office.location +"', telephone = '" + req.body.office.telephone +"', roomNumber = "+ req.body.office.roomNumber +"\n WHERE employeeID = "+req.body.adminID + ";\n";  
-                        employeeSQL+= "UPDATE warehousemember \n SET warehouseID = " + req.body.warehouseID + "\n WHERE memberID = " + req.body.adminID + ";\n";
-                        employeeSQL+= "COMMIT; ";
-                        DB.query(employeeSQL, (err)=>{
-                            if (err) throw err; 
+                let employeeSQL="";
+                if (result=="")    
+                    res.send({ 
+                        "status": "ACC DOESN't EXIST", 
+                        "err": true
+                    }); 
+                else{
+                    const checkLocationSQL = "SELECT * FROM office WHERE roomNumber = " +req.body.office.roomNumber+" AND location = '" + req.body.office.location +"' AND employeeID !=" +req.body.adminID;
+                    DB.query(checkLocationSQL, (err,result)=>{
+                        if (err) throw err;
+                        if (result!="")
                             res.send({
-                                "status": "SUCCESS", 
-                                "err": false
+                                "status": "DUPLICATE LOC", 
+                                "err": true 
                             });
+                        else{ 
+                            const checkEmailSQL = "SELECT employeeID FROM employee WHERE email ='" +req.body.email +"' AND employeeID !="+req.body.adminID;
+                            DB.query(checkEmailSQL, (err, result)=>{ 
+                            if (err) throw err;  
+                            if(result[0]!=undefined)
+                                res.send({
+                                    "status": "DUPLICATE EMAIL", 
+                                    "err": true
+                                });
+                            else{
+                                employeeSQL+= "START TRANSACTION; \n" 
+                                employeeSQL+= "UPDATE employee \n SET phoneNumber = '" + req.body.phoneNumber + "', password = '"+ req.body.password +"'\n WHERE employeeID = "+req.body.adminID + ";\n";
+                                employeeSQL+= "UPDATE employeeupdate \n SET updatedBy = " + req.body.employeeID + ", lastUpdate = '"+ time.getDateTime() +"'\n WHERE employeeID = "+req.body.adminID + ";\n";
+                                employeeSQL+= "UPDATE office \n SET location = '" + req.body.office.location +"', telephone = '" + req.body.office.telephone +"', roomNumber = "+ req.body.office.roomNumber +"\n WHERE employeeID = "+req.body.adminID + ";\n";  
+                                employeeSQL+= "UPDATE warehousemember \n SET warehouseID = " + req.body.warehouseID + "\n WHERE memberID = " + req.body.adminID + ";\n";
+                                employeeSQL+= "COMMIT; ";
+                                DB.query(employeeSQL, (err)=>{
+                                    if (err) throw err; 
+                                    res.send({
+                                        "status": "SUCCESS", 
+                                        "err": false
+                                    });
+                                });
+                            }
                         });
-                    }
-                });
+                        }
+                    });
                 }
             });
         }
@@ -129,13 +152,24 @@ router.post("/modifyAdministrator",urlEncodedParser, (req, res) => {
 });
 
 //view list of Administrators 
-router.get("/viewadministrator", (res) =>{
-    let SQL = "SELECT AD.*,\n ADof.location,ADof.roomNumber,ADof.telephone,\n ADup.updatedBy,ADup.lastUpdate\n FROM employee AD\n";
-    SQL += "INNER JOIN employeeupdate ADup\n ON ADM.employeeID = ADup.employeeID AND AD.role='AD'";
-    SQL += "INNER JOIN office ADof\n ON AD.employeeID  = ADof.employeeID AND AD.role='AD'";
-    DB.query(SQL, (err,result)=>{
+router.get("/viewAdministrators", (res) =>{
+    //check if this admin is an owner
+    DB.query(ownerCheck(req.query.employeeID), (err, result)=>{
         if (err) throw err;
-        res.send(result);
+        if (!result[0].IsOwner)
+        res.send({
+            "status": "NOT OWNER", 
+            "err": true 
+        });
+        else{
+            let SQL = "SELECT AD.*,\n ADof.location,ADof.roomNumber,ADof.telephone,\n ADup.updatedBy,ADup.lastUpdate\n FROM employee AD\n";
+            SQL += "INNER JOIN employeeupdate ADup\n ON ADM.employeeID = ADup.employeeID AND AD.role='AD'";
+            SQL += "INNER JOIN office ADof\n ON AD.employeeID  = ADof.employeeID AND AD.role='AD'";
+            DB.query(SQL, (err,result)=>{
+                if (err) throw err;
+                res.send(result);
+            });
+        }
     });
 });
 
